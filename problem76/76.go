454093497a936da8ce2c4567bcc1da655fc38032
Samer Masterson <samer@samertm.com>
2014-12-03 13:01:41 -0800

    speed up 76 a bajillion times

--- problem76/76.go
+++ problem76/76.go
@@ -2,13 +2,21 @@ package problem76
 
 import "strconv"
 
+type tuple struct{ x, y int }
+
+var cache = make(map[tuple]int)
+
 func cycle(x int, i int) (num int) {
-	if x == 0 {
+	if v, ok := cache[tuple{x, i}]; ok {
+		return v
+	} else if x == 0 {
 		return 1
 	} else if x < 0 || i <= 0 {
 		return 0
 	}
-	return cycle(x-i, i) + cycle(x, i-1)
+	val := cycle(x-i, i) + cycle(x, i-1)
+	cache[tuple{x, i}] = val
+	return val
 }
 
 func Solve() string {
